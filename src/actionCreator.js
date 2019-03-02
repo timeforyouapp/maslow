a4a9ca5b8c67199661fc46dde81863b7fdfcd5e9
Guilherme Diego <guidiego.expgames@gmail.com>
2019-03-01 21:47:35 -0300

    Add: Action Creator

--- src/actionCreator.js
+++ src/actionCreator.js
@@ -1,28 +1,32 @@
-export const x = (a,b) => a + b;
-export default x;
-// export const ActionCreator = (name, types, api = {}, customActions = {}) => ({
-//     [`_set${name}`]: (item) => ({
-//         type: types.set,
-//         payload: item,
-//     }),
-//     [`_setAll${name}`]: (items) => ({
-//         type: types.setAll,
-//         payload: items,
-//     }),
-//     [`_clear${name}State`]: () => ({
-//         type: types.clearState
-//     }),
-//     [`get${name}Detail`]: (apiParams) => ({
-//         type: types.getDetail,
-//         api: api.getDetail(apiParams),
-//     }),
-//     [`get${name}List`]: (apiParams) => ({
-//         type: types.getListApiActions,
-//         api: api.getList(apiParams),
-//     }),
-//     [`save${name}`]: (id, item) => ({
-//         type: typeof id !== 'object' ? types.updateApiActions :  types.createApiActions,
-//         api: typeof id !== 'object' ? api.update(id, apiParams) : api.create(id),
-//     }),
-//     ...customActions
-// })
+export const ActionCreator = (name, types, api, customActions = {}) => ({
+    [`_set${name}`]: (item) => ({
+        type: types.set,
+        payload: item,
+    }),
+    [`_setAll${name}`]: (items) => ({
+        type: types.setAll,
+        payload: items,
+    }),
+    [`_clear${name}State`]: () => ({
+        type: types.clearState
+    }),
+    [`get${name}Detail`]: (id) => ({
+        type: types.getDetail,
+        api: api.getDetail(id),
+    }),
+    [`get${name}List`]: (queryParams) => ({
+        type: types.getList,
+        api: api.getList(queryParams),
+    }),
+    [`save${name}`]: (id, item) => ({
+        type: item ? types.update :  types.create,
+        api: item ? api.update(id, item) : api.create(id),
+    }),
+    [`delete${name}`]: (id) => ({
+        type: types.delete,
+        api: api.delete(id),
+    }),
+    ...customActions
+});
+
+export default ActionCreator;
--- tests/actionCreator.spec.js
+++ tests/actionCreator.spec.js
@@ -1,7 +1,111 @@
-import x from '../src/actionCreator';
+import ActionCreator from '../src/actionCreator';
 
 describe('actionCreator', () => {
-   it('d', () => {
-       expect(x(1,2)).toBe(3);
-   });
+    const fakeName = 'Fake';
+    const fakeEntry = 'fakeEntry';
+    const fakeEntry2 = 'fakeEntry2';
+
+    let fakeDomainTypes = null;
+    let fakeTypes = null;
+    let fakeApi = null;
+
+    const checkAttributesAndExecFunctions = (actions) => {
+        let result = null;
+        expect(actions).toHaveProperty(`_set${fakeName}`);
+        expect(actions).toHaveProperty(`_setAll${fakeName}`);
+        expect(actions).toHaveProperty(`_clear${fakeName}State`);
+        expect(actions).toHaveProperty(`get${fakeName}Detail`);
+        expect(actions).toHaveProperty(`get${fakeName}List`);
+        expect(actions).toHaveProperty(`save${fakeName}`);
+        expect(actions).toHaveProperty(`delete${fakeName}`);
+
+        result = actions[`_set${fakeName}`](fakeEntry)
+        expect(result).toHaveProperty('type', fakeTypes.set);
+        expect(result).toHaveProperty('payload', fakeEntry);
+
+        result = actions[`_setAll${fakeName}`](fakeEntry)
+        expect(result).toHaveProperty('type', fakeTypes.setAll);
+        expect(result).toHaveProperty('payload', fakeEntry);
+
+        result = actions[`_clear${fakeName}State`](fakeEntry)
+        expect(result).toHaveProperty('type', fakeTypes.clearState);
+
+        result = actions[`get${fakeName}Detail`](fakeEntry)
+        expect(result).toHaveProperty('type', fakeTypes.getDetail);
+        expect(fakeApi.getDetail).toHaveBeenCalledTimes(1);
+        expect(fakeApi.getDetail).toHaveBeenCalledWith(fakeEntry);
+
+        result = actions[`get${fakeName}List`](fakeEntry)
+        expect(result).toHaveProperty('type', fakeTypes.getList);
+        expect(fakeApi.getList).toHaveBeenCalledTimes(1);
+        expect(fakeApi.getList).toHaveBeenCalledWith(fakeEntry);
+
+        result = actions[`save${fakeName}`](fakeEntry)
+        expect(result).toHaveProperty('type', fakeTypes.create);
+        expect(fakeApi.create).toHaveBeenCalledTimes(1);
+        expect(fakeApi.create).toHaveBeenCalledWith(fakeEntry);
+
+        result = actions[`save${fakeName}`](fakeEntry, fakeEntry2)
+        expect(result).toHaveProperty('type', fakeTypes.update);
+        expect(fakeApi.update).toHaveBeenCalledTimes(1);
+        expect(fakeApi.update).toHaveBeenCalledWith(fakeEntry, fakeEntry2);
+
+        result = actions[`delete${fakeName}`](fakeEntry)
+        expect(result).toHaveProperty('type', fakeTypes.delete);
+        expect(fakeApi.delete).toHaveBeenCalledTimes(1);
+        expect(fakeApi.delete).toHaveBeenCalledWith(fakeEntry);
+    };
+    
+    beforeEach(() => {
+        fakeDomainTypes = {
+            success: 'success',
+            fetching: 'fetching',
+            error: 'error',
+        };
+    
+        fakeTypes = {
+            setAll: 'setAll',
+            set: 'set',
+            clearState: 'clearState',
+            getDetail: fakeDomainTypes,
+            getList: fakeDomainTypes,
+            update: fakeDomainTypes,
+            create: fakeDomainTypes,
+            delete: fakeDomainTypes,
+        };
+    
+        fakeApi = {
+            getDetail: jest.fn(),
+            getList: jest.fn(),
+            update: jest.fn(),
+            create: jest.fn(),
+            delete: jest.fn(),
+        }
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
+
+    it('Check base props', () => {
+        const actions = ActionCreator(fakeName, fakeTypes, fakeApi);
+        checkAttributesAndExecFunctions(actions);
+    });
+
+    it('Check custom props', () => {
+        const newFakeActions = 'newFakeActions';
+        const actions = ActionCreator(fakeName, fakeTypes, fakeApi, {
+            [newFakeActions]: (x) => ({
+                type: newFakeActions,
+                payload: x
+            })
+        });
+
+        checkAttributesAndExecFunctions(actions);
+        
+        const result = actions[newFakeActions](fakeEntry);
+        expect(actions).toHaveProperty(newFakeActions);
+        expect(result).toHaveProperty('type', newFakeActions);
+        expect(result).toHaveProperty('payload', fakeEntry);
+    });
 });
