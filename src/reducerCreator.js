1be739b6cd2d2262f6b24a3c4edd7297f6e58517
Guilherme Diego <guidiego.expgames@gmail.com>
2019-03-02 01:18:10 -0300

    Add: Reducer Generator

--- src/reducerCreator.js
+++ src/reducerCreator.js
@@ -1,46 +1,65 @@
-export const ReducerCreator = (name, types, initialState = {}, customReducers = {}) => {
-    const _reducerMap = {
-      [types.setType]: (state, payload) => ({
-        ...state,
-        detail: action.payload
-      }),
-      [types.setAll]: (state, payload) => ({
-        ...state,
-        list: action.payload,
-      }),
-      [types.clearState]: (state, payload) => {
-        return Object.clone(initialState);
-      },
-      [types.domainFetching]: (state) => ({
-        fetching: true,
-      }),
-      [types.domainFetchSuccess]: (state, payload) => ({
-        fetching: false,
-      }),
-      [types.domainFetchError]: (state, payload) => ({
-        fetching: false,
-        errors: payload,
-      })
-    }
-    
-    _reducerMap[types.getDetailActions.success] = _reducerMap[types.setType]
-    _reducerMap[types.getListActions.success] = _reducerMap[types.setAllType]
-    _reducerMap[types.saveActions.success] = _reducerMap[types.setType]
-    _reducerMap[types.deleteActions.success] = _reducerMap[types.clearState]
-    
-    const reducerMap = {
-      ..._reducerMap,
-      ...customReducers,
-    }
-  
-    return (state = initialState, action) => {
-      const reducer = reducerMap[action.type];
-      
-      if (reducer) {
-        return reducer(state, action.payload)
-      }
-      
-      return state;
-    }
+import clone from 'fast-clone';
+
+const baseInitialState = {
+  fetching: false,
+  errors: [],
+  detail: {},
+  list: [],
+};
+
+export const ReducerCreator = (types, customInitialState = {}, customReducers = {}) => {
+  const initialState = clone({
+    ...baseInitialState,
+    ...customInitialState,
+  });
+
+  const _reducerMap = {
+    [types.set]: (state, payload) => ({
+      ...state,
+      detail: payload
+    }),
+    [types.setAll]: (state, payload) => ({
+      ...state,
+      list: payload,
+    }),
+    [types.clearState]: () => {
+      return clone(initialState);
+    },
+    [types.domainFetching]: (state) => ({
+      ...state,
+      fetching: true,
+    }),
+    [types.domainFetchSuccess]: (state) => ({
+      ...state,
+      fetching: false,
+    }),
+    [types.domainFetchError]: (state, payload) => ({
+      ...state,
+      fetching: false,
+      errors: payload,
+    })
   }
   
+  _reducerMap[types.getDetail.success] = _reducerMap[types.setType]
+  _reducerMap[types.getList.success] = _reducerMap[types.setAllType]
+  _reducerMap[types.create.success] = _reducerMap[types.setType]
+  _reducerMap[types.update.success] = _reducerMap[types.setType]
+  _reducerMap[types.delete.success] = _reducerMap[types.clearState]
+  
+  const reducerMap = {
+    ..._reducerMap,
+    ...customReducers,
+  }
+
+  return (state = clone(initialState), action) => {
+    const reducer = reducerMap[action.type];
+
+    if (reducer) {
+      return reducer(state, action.payload)
+    }
+    
+    return state;
+  }
+}
+
+export default ReducerCreator;
--- tests/reducerCreator.spec.js
+++ tests/reducerCreator.spec.js
@@ -1,7 +1,86 @@
-// const x = require('../src/actionCreator');
+import clone from 'fast-clone';
+import ReducerCreator from '../src/reducerCreator';
+import { FAKE_TYPES } from './helpers/mocks';
 
 describe('reducerCreator', () => {
-    it('d', () => {
-        expect(true).toBe(true);
+    let fakeTypes = null;
+
+    const checkInitialState = (state) => {
+        expect(state).toHaveProperty('fetching', false);
+        expect(state).toHaveProperty('detail', {});
+        expect(state).toHaveProperty('list', []);
+        expect(state).toHaveProperty('errors', []);
+    }
+
+    beforeEach(() => {
+        fakeTypes = clone(FAKE_TYPES);
+    });
+
+    it('check reducer with default props', () => {
+        const reducer = ReducerCreator(fakeTypes);
+        checkInitialState(reducer(undefined, {}))
+    });
+
+    it('check reducer default set and clear fns', () => {
+        const reducer = ReducerCreator(fakeTypes);
+        const fakePayload = 'fakePayload';
+
+        const state = reducer(undefined, {});
+        const newState = reducer(state, {
+            type: fakeTypes.set,
+            payload: fakePayload
+        })
+
+        const clearState = reducer(newState, {
+            type: fakeTypes.clearState,
+        })
+
+        checkInitialState(state);
+        checkInitialState(clearState);
+        expect(newState).toHaveProperty('detail', fakePayload);
+    });
+
+    it('check reducer default setAll fn', () => {
+        const reducer = ReducerCreator(fakeTypes);
+        const fakePayload = 'fakePayload';
+
+        const state = reducer(undefined, {});
+        const newState = reducer(state, {
+            type: fakeTypes.setAll,
+            payload: fakePayload
+        })
+
+        checkInitialState(state);
+        expect(newState).toHaveProperty('list', fakePayload);
+    });
+
+    it('check reducer default domainFetchError fn', () => {
+        const reducer = ReducerCreator(fakeTypes);
+        const fakePayload = 'fakePayload';
+
+        const state = reducer(undefined, {});
+        const newState = reducer(state, {
+            type: fakeTypes.domainFetchError,
+            payload: fakePayload
+        })
+
+        checkInitialState(state);
+        expect(newState).toHaveProperty('errors', fakePayload);
+    });
+
+    it('check reducer default domainFetching, domainFetchSuccess fns', () => {
+        const reducer = ReducerCreator(fakeTypes);
+
+        const state = reducer(undefined, {});
+        const fetchingState = reducer(state, {
+            type: fakeTypes.domainFetching,
+        })
+        const fetchSuccessState = reducer(fetchingState, {
+            type: fakeTypes.domainFetchSuccess,
+        })
+
+        checkInitialState(state);
+        expect(fetchingState).toHaveProperty('fetching', true);
+        expect(fetchSuccessState).toHaveProperty('fetching', false);
     });
  });
