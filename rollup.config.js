c76604f580e5b96ebdcc2894fafc7cf1f204b5ad
Guilherme Diego <guidiego.expgames@gmail.com>
2019-03-06 18:34:15 -0300

    Update: Structure

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,9 @@ import babel from 'rollup-plugin-babel';
 export default {
     input: 'src/index.js',
     output: {
-        name: 'freud',
+        name: 'maslow',
         file: 'dist/bundle.js',
         format: 'umd'
-    },
-    moduleContext: {
-        'node_modules/whatwg-fetch/fetch.js': 'window'
     },
     plugins: [
         resolve(),
