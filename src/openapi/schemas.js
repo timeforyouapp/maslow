9ee6a56bd11ca8077aaeb09723f8b951fd59813a
Guilherme Diego <guidiego.expgames@gmail.com>
2019-03-14 11:21:44 -0300

    Add: API Schema based on OpenApi

---
+++ src/openapi/schemas.js
@@ -0,0 +1,108 @@
+import Schema from 'node-schema';
+import str from 'string-validator';
+
+export const createNodeSchema = ({ properties, required }) => {
+    const schema = {};
+
+    Object.keys(properties).forEach((prop) => {
+        const propInfo = properties[prop];
+
+        if (propInfo.readOnly) {
+            return;
+        }
+
+        schema[prop] = {};
+
+        if (propInfo.maxLength) {
+            const minLength = propInfo.minLength || 0;
+            schema[prop].invalidLength = str.isLength(minLength, propInfo.maxLength);
+        }
+
+        if (~required.indexOf(prop)) {
+            schema[prop].required = (value) => typeof value === 'string' && value !== '';
+        }
+    });
+
+    return schema;
+};
+
+export const MaslowSchema = function (definition) {
+    const schemaItems = createNodeSchema(definition);
+    const methods = {
+        parse: (values) => values
+    };
+
+    function initSchema(values, data) {
+        const innerValues = {};
+        const innerMethods = {};
+        let next = null;
+        let prev = data ? data.__prev__ : null;
+
+        Object.keys(definition.properties).forEach((key) => {
+            innerValues[key] = values[key];
+        });
+
+        Object.keys(methods).forEach((key) => {
+            innerMethods[key] = (...parameters) => {
+                return methods[key].call({
+                    ...innerValues,
+                    ...innerMethods,
+                }, ...parameters);
+            };
+        });
+
+        function setInnerValue(prop, value) {
+            if (definition.properties[prop].readOnly) {
+                throw 'read only prop';
+            }
+
+            innerValues[prop] = value;
+        }
+
+        return Object.freeze({
+            ...innerValues,
+            ...innerMethods,
+            prev: function () {
+                return prev;
+            },
+            next: function () {
+                return next;
+            },
+            set: function (prop, value) {
+                setInnerValue(prop, value);
+                next = initSchema(innerValues, { __prev__: this });
+                return next;
+            },
+            update: function (newValues) {
+                Object.keys(definition.properties).forEach((prop) => {
+                    if (newValues[prop]) {
+                        setInnerValue(prop, newValues[prop]);
+                    }
+                });
+
+                next = initSchema(innerValues, { __prev__: this });
+                return next;
+            },
+            extractValues: function () {
+                return methods.parse({...innerValues});
+            },
+            validate: function () {
+                const s = Schema(schemaItems);
+                return s.validate(innerValues);
+            }
+        });
+    }
+
+    initSchema.addMethod = function (methodName, callback) {
+        if (typeof callback !== 'function') {
+            throw 'not function';
+        }
+
+        methods[methodName] = callback;
+    };
+
+    return initSchema;
+};
+
+
+export default MaslowSchema;
---
+++ tests/openapi/schemas.spec.js
@@ -0,0 +1,170 @@
+import {createNodeSchema, MaslowSchema} from '../../src/openapi/schemas';
+
+describe('MaslowSchema', () => {
+    let User = null;
+    const id = 'foo';
+    const name = 'bar';
+    const notAllowedProp = 'fizz';
+    const fakeDefinition = {
+        "properties": {
+            "created_at": {
+                "format": "date-time",
+                "readOnly": true,
+                "type": "string",
+                "x-nullable": true
+            },
+            "email": {
+                "maxLength": 200,
+                "type": "string"
+            },
+            "id": {
+                "maxLength": 36,
+                "readOnly": true,
+                "type": "string"
+            },
+            "name": {
+                "maxLength": 200,
+                "type": "string"
+            },
+            "password": {
+                "maxLength": 256,
+                "type": "string"
+            },
+            "notMaxLength": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "email",
+            "name",
+            "password"
+        ],
+        "type": "object"
+    }
+
+    beforeEach(() => {
+        User = MaslowSchema(fakeDefinition);
+    });
+
+    it('createNodeSchema createNodeSchema() return correct validations', () => {
+        const fakeSchema = createNodeSchema(fakeDefinition);
+
+        expect(fakeSchema).not.toHaveProperty('id');
+        expect(fakeSchema).not.toHaveProperty('created_at');
+
+        fakeDefinition.required.forEach((key) => {
+            expect(fakeSchema[key]).toHaveProperty('invalidLength');
+            expect(fakeSchema[key]).toHaveProperty('required');
+        });
+    });
+
+    it('should MaslowSchema return with correct values', () => {
+        const user = User({ name, id, notAllowedProp });
+
+        expect(user.id).toBe(id);
+        expect(user.name).toBe(name);
+        expect(user).not.toHaveProperty('notAllowedProp');
+
+        [
+            'set',
+            // 'method',
+            'extractValues',
+            'validate',
+            'update',
+            'parse',
+            'prev',
+            'next',
+        ].forEach((prop) => {
+            expect(user).toHaveProperty(prop);
+        });
+    });
+
+    it('should .set, .next, .prev methods work correcty', () => {
+        const user = User({ name, id, notMaxLength: notAllowedProp });
+        const altName = 'fuzz';
+        const newUser = user.set('name', altName);
+
+        expect(() => user.set('id', 2)).toThrow('read only prop');
+
+        expect(user).toHaveProperty('name', name);
+        expect(newUser).toHaveProperty('name', altName);
+        expect(newUser.prev()).toEqual(user);
+        expect(user.next()).toEqual(newUser);
+        expect(user.prev()).toBe(null);
+    });
+
+    it('should .extractValues work correctly', () => {
+        const user = User({ name, id, notAllowedProp });
+        const values = user.extractValues();
+
+        expect(values).toHaveProperty('name', name);
+        expect(values).toHaveProperty('id', id);
+        expect(values).not.toHaveProperty('notAllowedProp');
+    });
+
+    it('should .method add a new method', () => {
+        const value = 'fizz';
+
+        User.addMethod('foo', function () {
+            this.name = value;
+            return this.name + value;
+        });
+
+        const user = User({ name, id, notAllowedProp });
+
+        expect(user.foo()).toBe(value + value);
+        expect(user.name).toBe(name);
+    });
+
+    it('should throw exception to .method adding a not function', () => {
+        ['text', 1, 1.1, true, undefined, null, {}, []].forEach((entry) => {
+            expect(() => User.addMethod('foo', entry)).toThrow('not function');
+        });
+    });
+
+    it('should .parse change .extractValues', () => {
+        const prefix = 'Sr.';
+
+        User.addMethod('parse', function (values) {
+            return {
+                ...values,
+                name: `${prefix} ${values.name}`,
+            }
+        });
+
+        const user = User({ name, id, notAllowedProp });
+
+        expect(user.extractValues()).toHaveProperty('name', `${prefix} ${name}`);
+        expect(user).toHaveProperty('name', name);
+    });
+
+    it('should .update make a bulk .set', () => {
+        const altName = 'fizz';
+        const password = 'fuzz';
+        const newPass = 'fizzfuzz';
+
+        const user = User({ name, id, password });
+        const newUser = user.update({ name: altName, password: newPass });
+
+        expect(() => {
+            user.update({ id: 3, name: altName, password: newPass });
+        }).toThrow('read only prop');
+
+        expect(user).toHaveProperty('name', name);
+        expect(user).toHaveProperty('password', password);
+        expect(newUser).toHaveProperty('name', altName);
+        expect(newUser).toHaveProperty('password', newPass);
+        expect(newUser.prev()).toEqual(user);
+        expect(user.next()).toEqual(newUser);
+        expect(user.prev()).toBe(null);
+    });
+
+    it('should .validate work as expected', () => {
+        const user = User({ name, id, notAllowedProp });
+
+        return user.validate().then((errors) => {
+            expect(errors).toHaveProperty('email');
+            expect(errors).toHaveProperty('password');
+        });
+    });
+})
